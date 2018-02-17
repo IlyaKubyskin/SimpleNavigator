032ca36bfd53b6556a7a5f0a57cb441e732a748c
IlyaKubyshkin <ilkubik@yandex.ru>
2018-02-17 18:29:30 +0300

    it's alive! (a little)

--- app/src/main/java/com/kubik/simplenavigator/MapsActivity.java
+++ app/src/main/java/com/kubik/simplenavigator/MapsActivity.java
@@ -3,9 +3,14 @@ package com.kubik.simplenavigator;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,10 +18,21 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.maps.android.PolyUtil;
+import com.kubik.simplenavigator.pojo.Direction;
+import com.kubik.simplenavigator.pojo.Route;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.util.List;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, OnDirectionFoundListener {
 
     private GoogleMap mMap;
+
+    Button findPath;
+    EditText origin;
+    EditText destination;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +42,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        findPath = findViewById(R.id.btnFindPath);
+        origin = findViewById(R.id.etOrigin);
+        destination = findViewById(R.id.etDestination);
+
+        findPath.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendRequest();
+            }
+        });
+    }
+
+    private void sendRequest() {
+
+        String orig = String.valueOf(origin.getText());
+        String dest = String.valueOf(destination.getText());
+
+        DirectionFinder finder = new DirectionFinder(orig, dest, this);
+        finder.findDeWei();
+        List<Route> routes = finder.getRoutes();
+
     }
 
 
@@ -50,5 +88,23 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
         mMap.setMyLocationEnabled(true);
+    }
+
+    @Override
+    public void onDirectionFinderStart() {
+        Toast.makeText(this, "startSearch", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDirectionFinderSuccess(Direction direction) {
+        PolylineOptions options = new PolylineOptions()
+                .geodesic(true)
+                .color(Color.BLUE)
+                .width(10);
+
+
+        List<LatLng> list = PolyUtil.decode(direction.getRoutes().get(0).getOverviewPolyline().getPoints());
+        options.addAll(list);
+        mMap.addPolyline(options);
     }
 }
